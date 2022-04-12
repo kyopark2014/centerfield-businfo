dc4781b504ff5663b1bc22329979730099e20bd7
ksdyb.park <ksdyb@amazon.com>
2022-04-12 21:18:27 +0900

    update

---
+++ cdk/cdk.out/asset.7bf10245e5d9a72d56f2144a5a390340b3502fd6f57578b06db0fe35c2979320/index.js
@@ -0,0 +1,154 @@
+const aws = require('aws-sdk');
+const sns = new aws.SNS();
+const request = require('request');
+const convert = require('xml-js');
+
+const dynamo = new aws.DynamoDB.DocumentClient();
+
+const tableName = process.env.tableName;
+const topicArn = process.env.topicArn;
+
+exports.handler = async (event, context) => {
+    console.log('## ENVIRONMENT VARIABLES: ' + JSON.stringify(process.env))
+    console.log('## EVENT: ' + JSON.stringify(event))    
+    
+    const body = Buffer.from(event["body-json"], "base64");
+    const userReq = JSON.parse(body);
+
+    console.log('userReq: '+JSON.stringify(userReq));
+
+    const stationId = userReq['stationId'];
+    const routeId =  userReq['routeId'];
+    const staOrder = userReq['staOrder'];
+    const routeNumber = userReq['routeNumber'];
+    
+    const date = new Date();        
+    let timestamp = Math.floor(date.getTime()/1000).toString();
+
+	const options = { 
+        uri: 'http://openapi.gbis.go.kr/ws/rest/busarrivalservice', 
+        method: 'GET',
+        qs:{ 
+            serviceKey: '1234567890', // default
+            stationId: stationId,
+            routeId: routeId,
+            staOrder: staOrder
+        },
+    };
+    
+    let predictTime, plateNo, remainSeatCnt, message;
+    let statusCode;
+    let isCompleted = false;
+    makeRequest(options, function(responseCode,json){
+        if(responseCode==200) {
+            var res = JSON.parse(json);
+            console.log('json: '+JSON.stringify(res.response));
+
+            if(res.response.msgHeader.resultCode._text == '0') { // Success
+                predictTime = res.response.msgBody.busArrivalItem.predictTime1._text;
+                plateNo = res.response.msgBody.busArrivalItem.plateNo1._text;
+                remainSeatCnt =  res.response.msgBody.busArrivalItem.remainSeatCnt1._text;
+
+                console.log('No: '+plateNo+', predictTime: '+predictTime+', remainSeatCnt: '+remainSeatCnt);
+
+                message = predictTime+'분후에 '+routeNumber+'번 버스('+plateNo+') 버스가 도착할 예정입니다. '+' 현재 '+remainSeatCnt+'개의 빈좌석이 남아 있습니다.'; 
+
+                // putItem to DynamoDB
+                var putParams = {
+                    TableName: tableName,
+                    Item: {
+                        Timestamp: timestamp,
+                        RouteNumber: routeNumber,
+                        RouteId: routeId,
+                        PlateNo: plateNo,
+                        PredictTime: predictTime,
+                        RemainSeatCnt: remainSeatCnt    
+                    } 
+                };
+
+                dynamo.put(putParams, function(err){
+                    if (err) {
+                        console.log('Failure: '+err);
+                    } 
+                });
+            }
+            else { // Success but no bus
+                statusCode = '404';
+                console.log(statusCode+': '+res.response.msgHeader.resultMessage._text);
+
+                message = '현재 가능한 버스노선이 확인되지 않고 있습니다. 잠시후에 다시 시도해주시기 바랍니다.';; 
+            }
+
+            // publish
+            var snsParams = {
+                Subject: 'Bus 도착정보',
+                Message: message,        
+                TopicArn: topicArn
+            }; 
+            console.log('snsParams: '+JSON.stringify(snsParams));
+            
+            let snsResult = sns.publish(snsParams);
+            console.log('snsResult:', snsResult);
+
+            isCompleted = true;
+        }
+        else { // Failure to request
+            statusCode = responseCode;
+            console.log(statusCode+': '+responseCode);
+        }
+    });
+
+    function wait(){
+        return new Promise((resolve, reject) => {
+          if(!isCompleted) {
+            setTimeout(() => resolve("wait..."), 1000)
+          }
+          else {
+            setTimeout(() => resolve("wait..."), 0)
+          }
+        });
+    }
+    console.log(await wait());
+    console.log(await wait());
+    console.log(await wait());
+    console.log(await wait());
+    console.log(await wait());
+    
+    // for return info
+    const arrivalInfo = {
+        Timestamp: timestamp,
+        RouteNumber: routeNumber,
+        RouteId: routeId,
+        PlateNo: plateNo,
+        PredictTime: predictTime,
+        RemainSeatCnt: remainSeatCnt    
+    }; 
+    console.log('file info: ' + JSON.stringify(arrivalInfo)) 
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(arrivalInfo)
+    };
+    return response;
+};
+
+function makeRequest(options, callback) {
+    request(options, function (error, response, body) { 
+        let statusCode = response.statusCode;
+        if(!error && statusCode==200) {
+            // console.log('response: '+JSON.stringify(response));
+
+            statusCode = response.statusCode;
+
+            // console.log('xml: '+body);
+            var json = convert.xml2json(body, {compact: true, spaces: 4});
+
+            callback(statusCode,json);
+        }
+        else {
+            console.log('error: '+error);
+            
+            callback(statusCode,"");
+        }
+    });
+}
---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,163 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+
+const {SnsEventSource} = require('aws-cdk-lib/aws-lambda-event-sources');
+const sns = require('aws-cdk-lib/aws-sns');
+const lambda = require('aws-cdk-lib/aws-lambda');
+const apiGateway = require('aws-cdk-lib/aws-apigateway');
+const dynamodb = require('aws-cdk-lib/aws-dynamodb');
+const iam = require('aws-cdk-lib/aws-iam');
+const logs = require('aws-cdk-lib/aws-logs');
+
+const stage = "dev";
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // SNS
+    const topic = new sns.Topic(this, 'sns-centerfield', {
+      topicName: 'sns-centerfield'
+    });
+
+    // DynamoDB
+    let tableName = 'Centerfield-BusInfo';
+    const dataTable = new dynamodb.Table(this, 'Centerfield-BusInfo', {
+      tableName: tableName,
+      partitionKey: { name: 'RouteNumber', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'Timestamp', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // Lambda - getLocation
+    const lambdaGetLocation = new lambda.Function(this, "lambdaGetLocation", {
+      runtime: lambda.Runtime.NODEJS_14_X, 
+      code: lambda.Code.fromAsset("repositories/lambda-for-getLocation"), 
+      handler: "index.handler", 
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        topicArn: topic.topicArn,
+        tableName: tableName
+      }
+    });  
+    dataTable.grantReadWriteData(lambdaGetLocation);
+    topic.grantPublish(lambdaGetLocation);
+
+    // Lambda - Slack
+    const lambdaSlack = new lambda.Function(this, "LambdaSlack", {
+      runtime: lambda.Runtime.NODEJS_14_X, 
+      code: lambda.Code.fromAsset("repositories/lambda-for-slack"), 
+      handler: "index.handler", 
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        token: "xoxb-manually-updated"
+      }
+    });    
+    lambdaSlack.addEventSource(new SnsEventSource(topic)); 
+
+    // api Gateway
+    const logGroup = new logs.LogGroup(this, 'AccessLogs', {
+      retention: 90, // Keep logs for 90 days
+    });
+    logGroup.grantWrite(new iam.ServicePrincipal('apigateway.amazonaws.com')); 
+
+    const api = new apiGateway.RestApi(this, 'api-centerfield', {
+      description: 'api-centerfield',
+      endpointTypes: [apiGateway.EndpointType.REGIONAL],
+      binaryMediaTypes: ['*/*'], 
+      deployOptions: {
+        stageName: stage,
+        accessLogDestination: new apiGateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apiGateway.AccessLogFormat.jsonWithStandardFields({
+          caller: false,
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          user: true
+        }),
+      },
+      proxy: false
+    });   
+
+    lambdaGetLocation.grantInvoke(new iam.ServicePrincipal('apigateway.amazonaws.com'));
+
+    const templateString: string = `##  See http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+    ##  This template will pass through all parameters including path, querystring, header, stage variables, and context through to the integration endpoint via the body/payload
+    #set($allParams = $input.params())
+    {
+    "body-json" : $input.json('$'),
+    "params" : {
+    #foreach($type in $allParams.keySet())
+        #set($params = $allParams.get($type))
+    "$type" : {
+        #foreach($paramName in $params.keySet())
+        "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
+            #if($foreach.hasNext),#end
+        #end
+    }
+        #if($foreach.hasNext),#end
+    #end
+    },
+    "stage-variables" : {
+    #foreach($key in $stageVariables.keySet())
+    "$key" : "$util.escapeJavaScript($stageVariables.get($key))"
+        #if($foreach.hasNext),#end
+    #end
+    },
+    "context" : {
+        "account-id" : "$context.identity.accountId",
+        "api-id" : "$context.apiId",
+        "api-key" : "$context.identity.apiKey",
+        "authorizer-principal-id" : "$context.authorizer.principalId",
+        "caller" : "$context.identity.caller",
+        "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
+        "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
+        "cognito-identity-id" : "$context.identity.cognitoIdentityId",
+        "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
+        "http-method" : "$context.httpMethod",
+        "stage" : "$context.stage",
+        "source-ip" : "$context.identity.sourceIp",
+        "user" : "$context.identity.user",
+        "user-agent" : "$context.identity.userAgent",
+        "user-arn" : "$context.identity.userArn",
+        "request-id" : "$context.requestId",
+        "resource-id" : "$context.resourceId",
+        "resource-path" : "$context.resourcePath"
+        }
+    }`
+
+    const requestTemplates = { // path through
+      "application/json" : templateString
+    }
+
+    const getLocation = api.root.addResource('getLocation');
+    getLocation.addMethod('GET', new apiGateway.LambdaIntegration(lambdaGetLocation, {
+      PassthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_TEMPLATES,
+      requestTemplates: requestTemplates,
+      integrationResponses: [{
+        statusCode: '200',
+      }], 
+      proxy:false, 
+    }), {
+      methodResponses: [   // API Gateway sends to the client that called a method.
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': apiGateway.Model.EMPTY_MODEL,
+          }, 
+        }
+      ]
+    }); 
+    
+    new cdk.CfnOutput(this, 'apiUrl', {
+      value: api.url,
+      description: 'The url of API Gateway',
+    });    
+  }
+}
